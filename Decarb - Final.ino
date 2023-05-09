115a852f49e626ed25121494334e25a1817a2675
krankschaft <82898841+krankschaft@users.noreply.github.com>
2023-05-09 14:44:25 +0800

    Create Decarb - Final.ino

---
+++ Decarb - Final.ino
@@ -0,0 +1,65 @@
+/*
+
+ignITE Challenge 2023
+Decarbonisation at Home
+Programme Written by Freddy Tay
+
+Competition - Extra Attribute
+
+/*
+Change variable here
+*/
+
+//////////////////////////////////////////////////////////////////////
+
+int dist = 16; // in cm
+int delayt = 5; //in seconds
+int LEDPin = 11; // enter PIN number that controls the LED, identify the pin and make sure to connect to GND for the black wire
+
+////////////////////////////////////////////////////////////////////// 
+
+// defines pins numbers
+const int trigPin = 4;
+const int echoPin = 3;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(LEDPin, OUTPUT); // Sets the LEDPin as an Output
+  Serial.begin(9600); // Starts the serial communication
+
+  pinMode(9, OUTPUT); // Initialise communication with solenoid valve
+  
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if(distance > dist) {
+  digitalWrite(9, LOW);
+  digitalWrite(LEDPin, LOW);
+  }
+  else {
+  digitalWrite(9, HIGH);
+  digitalWrite(LEDPin, HIGH);
+  delay(delayt * 1000);
+  }
+  
+}
